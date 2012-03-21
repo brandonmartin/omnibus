2b9f82d5b91c867a083ea479bebc77716806c8d2
Brandon Martin <b@heyomayeah.com>
2012-03-21 13:49:48 -0700

    at this point libiconv works (love me some autoconf bustedness)

--- config/software/libiconv.clj
+++ config/software/libiconv.clj
@@ -27,18 +27,15 @@
 
 (software "libiconv" :source "libiconv-1.13.1"
           :steps [
-;;                  {:command (if (is-os? "freebsd") "perl" "true")
-;;                   :args [ "-pi" "-e" "s/(freebsd1|freebsd\\[123\\])\\*/$1\\.\\*/g" (str *omnibus-build-dir* "/libiconv-1.13.1/configure") ]}
-;;                  {:command (if (is-os? "freebsd") "perl" "true")
-;;                   :args [ "-pi" "-e" "s/(freebsd1|freebsd\\[123\\])\\*/$1\\.\\*/g" (str *omnibus-build-dir* "/libiconv-1.13.1/preload/configure") ]}
-;;                  {:command (if (is-os? "freebsd") "perl" "true")
-;;                   :args [ "-pi" "-e" "s/(freebsd1|freebsd\\[123\\])\\*/$1\\.\\*/g" (str *omnibus-build-dir* "/libiconv-1.13.1/libcharset/configure") ]}
-                  {:command (if (is-os? "freebsd") "cp" "true")
-                   :args [ "m4/eilseq.m4" "srcm4/eilseq.m4" ]}
+                  {:command (if (is-os? "freebsd") "perl" "true")
+                   :args [ "-pi" "-e" "s/(freebsd1|freebsd\\[\\[123\\]\\]|freebsd\\[\\[12\\]\\])\\*/$1\\.\\*/g" (str *omnibus-build-dir* "/libiconv-1.13.1/m4/libtool.m4") ]}
                   {:command (if (is-os? "freebsd") "sh" "true")
-                   :args [ "-c" "aclocal -I srcm4 --output=aclocal.m4 && touch aclocal.m4 && automake --add-missing --gnits srclib/Makefile && autoconf && autoheader" ]}
+                   :args [ "-c" "aclocal -I m4 -I srcm4 --output=aclocal.m4 && touch aclocal.m4 && automake --add-missing --gnits srclib/Makefile && autoconf && autoheader" ]}
                   {:command (if (is-os? "freebsd") "sh" "true")
-                   :args [ "-c" "cd libcharset && aclocal -I ../srcm4 --output=autoconf/aclocal.m4 && autoconf --include autoconf && autoheader --include autoconf && touch config.h.in" ]}
+                   :args [ "-c" "cd preload && aclocal -I ../m4 -I ../srcm4 && autoconf --include autoconf" ]}
+
+                  {:command (if (is-os? "freebsd") "sh" "true")
+                   :args [ "-c" "cd libcharset && aclocal -I ../m4 -I ../srcm4 --output=autoconf/aclocal.m4 && autoconf --include autoconf && autoheader --include autoconf && touch config.h.in" ]}
 
                   {:env {"CFLAGS" "-L/opt/opscode/embedded/lib -I/opt/opscode/embedded/include" "LD_RUN_PATH" "/opt/opscode/embedded/lib"}
                    :command "./configure" :args args }
